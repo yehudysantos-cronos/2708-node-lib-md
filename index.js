dcc5133e3cf73db885a4e497d432ada434b4964e
yehudysantos-cronos <yehudy.santos@escola.pr.gov.br>
2026-05-19 15:20:55 +0000

    TRABALHO 2 FINALIZADO

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.bgCyan('Quem tem A Luz já mais ficará na escuridão'));
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+//async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+         const encoding = 'utf-8';
+     const texto = awaitfs.promises.readFile(caminhoDoArquivo,encoding)
+     console.log(chalk.red(texto));
+    }  catch (erro) {
+        trataErro(erro)
+    }
+    
+}
+
+// promises com then()
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.promises
+//      .readFile(caminhoDoArquivo, encoding)
+//      .then((texto) => console.log(chalk.green(texto)))
+//      .catch(trataErro)
+//}
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => { 
+//        if (erro) {
+//            trataErro(erro);
+//        }
+//     console.log(chalk.green(texto));
+//    })
+//}
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
+
+//console.log(chalk.bgCyan('Quem tem A Luz já mais ficará na escuridão'));
+
